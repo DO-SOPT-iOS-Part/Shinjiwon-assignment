b2bae03cddce482aae3894d5f3462cbb12223234
shinzz410 <tlswldnjs410@gmail.com>
2023-10-16 22:01:49 +0900

    Setting#1 extension 추가

---
+++ Jiwon's Weather Forecast/Jiwon's Weather Forecast/Extension/UICollectionViewCell+.swift
@@ -0,0 +1,15 @@
+//
+//  UICollectionViewCell.swift
+//  Jiwon's Weather Forecast
+//
+//  Created by 신지원 on 2023/10/16.
+//
+
+import UIKit
+
+extension UICollectionViewCell{
+    
+    static var cellIdentifier : String {
+        return String(describing: self)
+    }
+}
---
+++ Jiwon's Weather Forecast/Jiwon's Weather Forecast/Extension/UITableViewHeaderFooterView+.swift
@@ -0,0 +1,15 @@
+//
+//  UITableViewFooterHeaderView+.swift
+//  Jiwon's Weather Forecast
+//
+//  Created by 신지원 on 2023/10/16.
+//
+
+import UIKit
+
+extension UITableViewHeaderFooterView {
+    
+    static var cellIdentifier  : String {
+        return String(describing: self)
+    }
+}
