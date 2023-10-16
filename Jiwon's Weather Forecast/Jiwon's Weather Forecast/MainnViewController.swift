e0e6fdea345c0f140d31c2571315ab8335067c4b
shinzz410 <tlswldnjs410@gmail.com>
2023-10-16 21:34:11 +0900

    Setting#1 프로젝트 생성

---
+++ Jiwon's Weather Forecast/Jiwon's Weather Forecast/MainnViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Jiwon's Weather Forecast
+//
+//  Created by 신지원 on 2023/10/16.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
